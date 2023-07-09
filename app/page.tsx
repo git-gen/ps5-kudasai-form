6b8891f3af8d601372904c4480d9d59e3cf97693
git-gen <gitkotobuki@gmail.com>
2023-07-09 15:26:55 +0900

    フォームを作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,116 @@
+'use client'
+
+import { useState, ChangeEvent } from 'react'
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
+import Button from '@/components/Button'
+import SelectHard from '@/components/SelectHard'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  // 0: 申請前
+  // 1: 確認画面
+  // 2: 申請完了
+  const stages = [0, 1, 2]
 
-      <div className={styles.center}>
+  const [isError, setIsError] = useState(false)
+  const [currentStage, setCurrentStage] = useState(0)
+  const [hard, setHard] = useState({
+    id: 0,
+    name: '',
+    image: ''
+  })
+  const [name, setName] = useState('')
+  const [comment, setComment] = useState('')
+
+  const handleStage = (stage: number) => {
+    if (hard && name && comment) {
+      setIsError(false)
+      setCurrentStage(stage)
+    } else {
+      setIsError(true)
+    }
+  }
+
+  const handleHard = (hard: {id: number, name: string, image: string}) => {
+    setHard(hard)
+  }
+
+  const handleName = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
+  }
+
+  const handleComment = (e: ChangeEvent<HTMLInputElement>) => {
+    setComment(e.target.value)
+  }
+
+  function ConfirmStage() {
+    return (
+      <div className={styles.form}>
+        <div className={styles.inputLabel}>欲しいハードを選択</div>
+        <div>{hard.name}</div>
+        <div className={styles.inputLabel}>名前</div>
+        <div>{name}</div>
+        <div className={styles.inputLabel}>ご要望</div>
+        <div>{comment}</div>
+      </div>
+    )
+  }
+
+  function CompleteStage() {
+    return (
+      <div className={styles.submitComplete}>
+        <div>申請が完了しました</div>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src='/check.svg'
+          alt='complete'
+          width={256}
+          height={256}
         />
       </div>
+    )
+  }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+  function ConfirmForm() {
+    if (currentStage === stages[1]) {
+      return <ConfirmStage />
+    } else {
+      return <CompleteStage />
+    }
+  }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+  function Buttons() {
+    if (currentStage === stages[0]) {
+      return (
+        <div className={styles.buttons}>
+          <Button text="内容を確認する" onClick={() => handleStage(stages[1])} />
+        </div>
+      )
+    } else if (currentStage === stages[1])  {
+      return (
+        <div className={styles.buttons}>
+          <Button text="内容を編集する" onClick={() => handleStage(stages[0])} />
+          <Button text="内容を送信する" onClick={() => handleStage(stages[2])} />
+        </div>
+      )
+    }
+  }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+  return (
+    <main className={styles.main}>
+      <div className={styles.header}>PS5くださいフォーム</div>
+      <div className={styles.container}>
+        {currentStage === stages[0] ?
+          <div className={styles.form}>
+            <div className={styles.inputLabel}>欲しいハードを選択</div>
+            <SelectHard select={hard} onChange={handleHard} />
+            <label htmlFor="name" className={styles.inputLabel}>名前</label>
+            <input id="name" type="text" value={name} onChange={handleName} className={styles.textInput} />
+            <label htmlFor="comment" className={styles.inputLabel}>ご要望</label>
+            <input id="comment" type="text" value={comment} onChange={handleComment} className={styles.textInput} />
+          </div>
+        : <ConfirmForm />}
+        <Buttons />
+        {isError && <div className={styles.errorMessage}>全ての項目を入力してください</div>}
       </div>
     </main>
   )
